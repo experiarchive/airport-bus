5c570ff079e19ad7f157bdb9bb3e651269b82b9a
Homin Kim <experiarchive@gmail.com>
2025-11-30 15:26:37 +0900

    Fix Adsense script warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,11 +60,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script
+        <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3505313580947440"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
         />
       </head>
       <body
